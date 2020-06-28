0bb4a2668e247a505d68674e2ce4ffee48f89515
kenriortega <kenriortega@gmail.com>
2020-06-28 09:29:40 -0400

    add change

--- api/get-weather.js
+++ api/get-weather.js
@@ -1,26 +1,27 @@
 const {
-    RCApiClient,
-    RCWeather,
-    RED_CUBA_SOURCE,
-    MUNICIPALITIES,
-    UtilsService,
-  } = require('cuba-weather-javascript')
+  RCApiClient,
+  RCWeather,
+  RED_CUBA_SOURCE,
+  MUNICIPALITIES,
+  UtilsService
+} = require('cuba-weather-javascript')
 
 module.exports = (req, res) => {
-  const name = req.query.name
-  let municipality = MUNICIPALITIES.find(
-    municipality => municipality.nameCured === name
-  )
-  let bestSource = UtilsService.getBestDistanceByMunicipality(
-    municipality,
-    RED_CUBA_SOURCE
-  )
+  res.json({ name: 'John', email: 'john@example.com' })
+  // const name = req.query.name
+  // let municipality = MUNICIPALITIES.find(
+  //   municipality => municipality.nameCured === name
+  // )
+  // let bestSource = UtilsService.getBestDistanceByMunicipality(
+  //   municipality,
+  //   RED_CUBA_SOURCE
+  // )
 
-  try {
-    let res = await RCApiClient.get(bestSource.name)
-    let weather = new RCWeather(res.data.data)
-    res.send(weather.weathertoOBJS())
-  } catch (err) {
-    res.send({err:"error"})
-  }
+  // try {
+  //   let res = await RCApiClient.get(bestSource.name)
+  //   let weather = new RCWeather(res.data.data)
+  //   res.send(weather.weathertoOBJS())
+  // } catch (err) {
+  //   res.send({err:"error"})
+  // }
 }
