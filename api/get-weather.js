8c94c3eec817daf42218657747b1a45ddf05bf39
kenriortega <kenriortega@gmail.com>
2020-06-28 09:33:52 -0400

    add logic to municipality

--- api/get-weather.js
+++ api/get-weather.js
@@ -7,21 +7,21 @@ const {
 } = require('cuba-weather-javascript')
 
 module.exports = (req, res) => {
-  res.json({ name: req.query, email: 'john@example.com' })
-  // const name = req.query.name
-  // let municipality = MUNICIPALITIES.find(
-  //   municipality => municipality.nameCured === name
-  // )
-  // let bestSource = UtilsService.getBestDistanceByMunicipality(
-  //   municipality,
-  //   RED_CUBA_SOURCE
-  // )
+  // res.json({ name: , email: 'john@example.com' })
 
-  // try {
-  //   let res = await RCApiClient.get(bestSource.name)
-  //   let weather = new RCWeather(res.data.data)
-  //   res.send(weather.weathertoOBJS())
-  // } catch (err) {
-  //   res.send({err:"error"})
-  // }
+  let municipality = MUNICIPALITIES.find(
+    municipality => municipality.nameCured === req.query.name
+  )
+  let bestSource = UtilsService.getBestDistanceByMunicipality(
+    municipality,
+    RED_CUBA_SOURCE
+  )
+
+  try {
+    let res = await RCApiClient.get(bestSource.name)
+    let weather = new RCWeather(res.data.data)
+    res.json(weather.weathertoOBJS())
+  } catch (err) {
+    res.send({err:"error"})
+  }
 }
