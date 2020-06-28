345fde4f8a1fd516112e208ed1b8ba0c7361ed58
kenriortega <kenriortega@gmail.com>
2020-06-28 08:21:37 -0400

    add: vercel functions to get weather from a municipality

--- api/name/get-weather.js
+++ api/name/get-weather.js
@@ -1,7 +1,30 @@
+const {
+    RCApiClient,
+    RCWeather,
+    RED_CUBA_SOURCE,
+    MUNICIPALITIES,
+    UtilsService,
+    IsmetWeather,
+    IsmetClient
+  } = require('cuba-weather-javascript')
+
 module.exports = (req, res) => {
   const {
     query: { name }
   } = req
+  let municipality = MUNICIPALITIES.find(
+    municipality => municipality.nameCured === name
+  )
+  let bestSource = UtilsService.getBestDistanceByMunicipality(
+    municipality,
+    RED_CUBA_SOURCE
+  )
 
-  res.send(`Hello ${name}!`)
+  try {
+    let res = await RCApiClient.get(bestSource.name)
+    let weather = new RCWeather(res.data.data)
+    res.send(weather.weathertoOBJS())
+  } catch (err) {
+    res.send(err)
+  }
 }
